aa687bdece8288b8fb47200e444d43e09ca4fe22
Александр <@DESKTOP-8NHVI7K>
2022-05-17 17:37:07 +0300

    Добавьте файлы проекта.

---
+++ PR11/PR11/Page1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PR11
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Tugolukova Daria PKS-30K, vsego xoroshego" }
+                }
+            };
+        }
+    }
+}
